13445d5de9cbfaee16e4ea8e45cfd2600180579e
d3j1x <d3j1x@yahoo.com>
2025-03-04 15:05:51 +0100

    update Home p

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,6 +10,16 @@
   "
 	/>
 </svelte:head>
+
 <div class="m-5 sm:m-10">
 	<img src={TestxBg} class="m-auto w-full max-w-5xl rounded-md" alt="T E S T X" />
 </div>
+<div class="my-10 text-center">
+	<h1 class="text-xl font-semibold">
+		"Kickstart your career in software testing with us and unlock endless opportunities for growth
+		and success."
+	</h1>
+</div>
+
+<style>
+</style>
